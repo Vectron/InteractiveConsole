c21182889f831280402e3b69cd03caf9cc64e2ed
Vectron <thijs_bloebaum@hotmail.com>
2023-06-26 15:57:13 +0200

    Supress unused usings in global usings.

--- tests/InteractiveConsole.Tests/Usings.cs
+++ tests/InteractiveConsole.Tests/Usings.cs
@@ -1,1 +1,3 @@
+#pragma warning disable IDE0005 // Using directive is unnecessary.
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
+#pragma warning restore IDE0005 // Using directive is unnecessary.
